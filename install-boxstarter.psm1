e586f7fbc51c425d7585fae98622ee3e66eb2eb9
Luke Barnett <luke.barnett@enlighten.co.nz>
2016-04-14 17:23:36 +1200

    Clean up

--- install-boxstarter.psm1
+++ install-boxstarter.psm1
@@ -4,36 +4,12 @@ function RunBoxstarter($scriptName) {
   Install-BoxstarterPackage -PackageName ($rootUrl + $scriptName) -DisableReboots
 }
 
-function Install-Boxstarter-ALl {
+function Install-Boxstarter-All {
   RunBoxstarter 'boxstarter.ps1'
 }
 
-function Install-Atom {
-  RunBoxstarter 'atom-profile.ps1'
-}
-
-function Install-DevTools {
-  RunBoxstarter 'dev-tools.ps1'
-}
-
-function Install-Essentials {
-  RunBoxstarter 'essentials.ps1'
-}
-
-function Install-Git {
-  RunBoxstarter 'git-tools.ps1'
-}
-
-function Install-Utilities {
-  RunBoxstarter 'utilities.ps1'
-}
-
-function Install-PersonalUtilities {
-  RunBoxstarter 'personal-utilities.ps1'
-}
-
-function Install-VisualStudio {
-  RunBoxstarter 'visualstudio.ps1'
+function Install-VisualStudio-Extensions {
+  RunBoxstarter 'visualstudio-extensions.ps1'
 }
 
 export-modulemember -function Install-*
